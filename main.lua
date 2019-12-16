5fb98abe7e274004e7c69c8a28d239b67d9bb898
lil_albertus <bart_rooyakkers@hotmail.com>
2019-12-16 13:35:20 +0100

    Indeling

--- main.lua
+++ main.lua
@@ -5,3 +5,13 @@
 -----------------------------------------------------------------------------------------
 
 -- Your code here
+
+-- 1 - Corona Engine Bibliotheken aanspreken
+
+-- 2 - Vaste variablen aanmaken
+
+-- 3 - Objecten aanmaken
+
+-- 4 - Functies
+
+-- 5 - Eventlisteners / andere interactieve zaken
